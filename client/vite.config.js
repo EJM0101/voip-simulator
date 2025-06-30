322bfbd9a53b1c8ce9d3dc69a20f7a38f232b8d3
Emman Mlmb <emmanjibi@gmail.com>
2025-06-30 13:49:21 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // équivalent à 0.0.0.0
+    port: process.env.PORT || 4173,
+    allowedHosts: ['voip-simulator.onrender.com']
+  }
 });
